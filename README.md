5c8842a55c42706645c5c626b5890f9b744dbe05
徐顺洁 <1051513344@qq.com>
2020-09-20 14:36:32 +0800

    SpringCloud客户端配置中心

--- README.md
+++ README.md
@@ -5,9 +5,8 @@ Feign调用，自带ribbon，默认轮询负载均衡规则
 * 1.先启动7001
 * 2.再启动7002
 * 3.再启动cloud-config-center3344
-* 4.访问 
-* http://localhost:3344/application-dev.yml 或
-* http://localhost:3344/application/dev/master
+* 4.再启动cloud-config-client3355
+* 5.访问 http://localhost:8084/configInfo
 
 
 
---
+++ cloud-config-client-3355/src/main/java/com/laoxu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.laoxu.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/serverPort")
+    public String getServerPort(){
+        return serverPort;
+    }
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
