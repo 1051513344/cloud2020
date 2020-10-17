8c67bb43ec3bdd45ed0273b47474a907d345b95f
1051513344 <1051513344@qq.com>
2020-10-17 15:07:45 +0800

    sentinel入门

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # cloud2020
 SpringCloud2020教程
 
-* 1.先启动9001
-* 2.再启动9002
-* 3.再启动83
-* 2.访问 http://localhost:83/consumer/payment/nacos/1
+* 1.启动nacos
+* 2.再启动sentinel
+* 3.再启动8041
+* 4.访问 http://localhost:8041/testA，http://localhost:8041/testB
+* 5.访问：http://localhost:8080查看sentinel监控情况
 
 
 
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/laoxu/springcloud/MainApp8041.java
@@ -0,0 +1,15 @@
+package com.laoxu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class MainApp8041 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8041.class, args);
+    }
+}
